00127f987f19308c8c98025e7c08261a66a2eb76
akingmingiam <133499710+akingmingiam@users.noreply.github.com>
2023-05-13 18:52:36 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include "datablob.hpp"
+using namespace std;
+
+int main(){
+    int *ma=new int[4]{1,2,3,4};
+    DataBlob a=DataBlob(2,2,1,INT,ma);
+    DataBlob b=a;
+    DataBlob c=a*b;
+    // cout<<a.at<int>(1,1,1) <<endl;
+    // cout<<b.at<int>(1,1,1) <<endl;
+    cout<<c.at<int>(0,0,0)<<endl;
+
+
+}
+
+
+
+
